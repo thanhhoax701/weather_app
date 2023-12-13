c3a39d19a532c9c63d110aef31cc9762927e1850
thanhhoax701 <tth1610.vl@gmail.com>
2023-12-13 11:46:37 +0700

    icon

--- src/App.js
+++ src/App.js
@@ -3,21 +3,40 @@ import axios from "axios";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 import Weather from "./Weather";
+import Modal from "react-modal";
 import "./App.css"; // Import CSS
 
 function App() {
   const [weatherData, setWeatherData] = useState(null);
+  // Du bao thoi tiet hien thi theo gio
+  const [hourlyWeatherData, setHourlyWeatherData] = useState(null);
   const [searchCity, setSearchCity] = useState("");
   const [loading, setLoading] = useState(false);
   const [currentDateTime, setCurrentDateTime] = useState(new Date());
+  // Show button Du bao theo gio
+  const [showHourlyWeather, setShowHourlyWeather] = useState(false);
 
   const getWeatherData = async (lat, lon) => {
     try {
       setLoading(true);
-      const response = await axios.get(
+
+      // Lấy thông tin thời tiết hiện tại
+      const currentWeatherResponse = await axios.get(
         `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=7b16a3bb0d4c6253ab56ca6a2a14f500&units=metric`
       );
-      setWeatherData(response.data);
+      setWeatherData(currentWeatherResponse.data);
+
+      // Lấy thông tin thời tiết theo từng giờ
+      const hourlyWeatherResponse = await axios.get(
+        `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=7b16a3bb0d4c6253ab56ca6a2a14f500&units=metric`
+      );
+
+      // Lọc dữ liệu thời tiết theo từng giờ từ dữ liệu forecast
+      const filteredHourlyData = hourlyWeatherResponse.data.list.filter(
+        (item) => item.dt_txt.includes("12:00:00")
+      );
+
+      setHourlyWeatherData(filteredHourlyData);
     } catch (error) {
       console.error("Error fetching weather data:", error);
     } finally {
@@ -25,15 +44,40 @@ function App() {
     }
   };
 
+  const getHourlyDataForDay = (data, day) => {
+    const startOfDay = new Date(day);
+    const endOfDay = new Date(day);
+    endOfDay.setDate(endOfDay.getDate() + 1);
+
+    return data.filter((item) => {
+      const itemDate = new Date(item.dt * 1000);
+      return itemDate >= startOfDay && itemDate < endOfDay;
+    });
+  };
+
   const handleSearch = async () => {
     try {
       setLoading(true);
-      const response = await axios.get(
-        `https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&appid=7b16a3bb0d4c6253ab56ca6a2a14f500&units=metric`
+      const [weatherResponse, hourlyWeatherResponse] = await Promise.all([
+        axios.get(
+          `https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&appid=7b16a3bb0d4c6253ab56ca6a2a14f500&units=metric`
+        ),
+        axios.get(
+          `https://api.openweathermap.org/data/2.5/forecast?q=${searchCity}&appid=7b16a3bb0d4c6253ab56ca6a2a14f500&units=metric`
+        ),
+      ]);
+
+      setWeatherData(weatherResponse.data);
+
+      // Lọc dữ liệu thời tiết theo từng giờ từ dữ liệu forecast
+      const filteredHourlyData = getHourlyDataForDay(
+        hourlyWeatherResponse.data.list,
+        new Date()
       );
-      setWeatherData(response.data);
+
+      setHourlyWeatherData(filteredHourlyData);
     } catch (error) {
-      console.error("Error fetching weather data:", error);
+      console.error("Error fetching data:", error);
     } finally {
       setLoading(false);
     }
@@ -49,6 +93,13 @@ function App() {
       alert('Geolocation is not supported by your browser.');
     }
   };
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleToggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
 
   useEffect(() => {
     // Lấy ngày và thời gian hiện tại mỗi giây
@@ -84,7 +135,9 @@ function App() {
           onClick={getCurrentLocation}
         />
       </h1>
-      <p>{getFormattedDate()} {currentDateTime.toLocaleTimeString()}</p>
+      <p>
+        {getFormattedDate()} {currentDateTime.toLocaleTimeString()}
+      </p>
       <div className="search-container">
         <input
           className="search_input"
@@ -93,10 +146,46 @@ function App() {
           value={searchCity}
           onChange={(e) => setSearchCity(e.target.value)}
         />
-        <button className="search_button" onClick={handleSearch}>Search</button>
+        <button className="search_button" onClick={handleSearch}>
+          Search
+        </button>
+        <button className="toggle_button" onClick={handleToggleModal}>
+          {isModalOpen ? "Hide Hourly Weather" : "Show Hourly Weather"}
+        </button>
       </div>
       {loading && <p>Loading...</p>}
       {weatherData && <Weather data={weatherData} />}
+      {showHourlyWeather && hourlyWeatherData && (
+        <div className="hourly-weather">
+          <h2>Hourly Weather</h2>
+          <ul>
+            {hourlyWeatherData.map((hourlyData) => (
+              <li key={hourlyData.dt}>
+                {new Date(hourlyData.dt * 1000).getHours()}:00 -{" "}
+                {Math.round(hourlyData.main.temp)}°C
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={handleToggleModal}
+        contentLabel="Hourly Weather Modal"
+        className="Modal"
+      >
+        <h2>Hourly Weather</h2>
+        <ul>
+          {hourlyWeatherData &&
+            hourlyWeatherData.map((hourlyData) => (
+              <li key={hourlyData.dt}>
+                {new Date(hourlyData.dt * 1000).getHours()}:00 -{" "}
+                {Math.round(hourlyData.main.temp)}°C
+              </li>
+            ))}
+        </ul>
+        <button onClick={handleToggleModal}>Close</button>
+      </Modal>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -2,13 +2,42 @@ import React from 'react';
 import './Weather.css'; // Import CSS
 
 const Weather = ({ data }) => {
+    const formatTime = (timestamp) => {
+        return new Date(timestamp * 1000).toLocaleTimeString('en-US', {
+            hour: 'numeric',
+            minute: 'numeric',
+            second: 'numeric',
+            hour12: true,
+        });
+    };
+
+    const getWeatherIcon = () => {
+        // Lấy tên của icon từ dữ liệu thời tiết thực tế
+        const icon = data.weather[0].icon;
+        // Tạo link đến icon từ API OpenWeatherMap
+        const iconUrl = `http://openweathermap.org/img/w/${icon}.png`;
+        // Trả về JSX hiển thị icon
+        return <img src={iconUrl} alt="Weather Icon" />;
+    };
+
     return (
         <div className="Weather">
-            <h2 className="weather_country">{data.name}, {data.sys.country}</h2>
-            <p className="weather_items">Temperature: {data.main.temp} °C</p>
-            <p className="weather_items">Weather: {data.weather[0].description}</p>
-            <p className="weather_items">Humidity: {data.main.humidity}%</p>
-            <p className="weather_items">Wind Speed: {data.wind.speed} m/s</p>
+            <h2 className="weather_country">
+                {getWeatherIcon()}
+                {data.name}, {data.sys.country}
+            </h2>
+            <div className="flex">
+                <p className="weather_items_one">Temperature: {Math.round(data.main.temp)} °C</p>
+                <p className="weather_items_one">Weather: {data.weather[0].description}</p>
+            </div>
+            <div className="flex">
+                <p className="weather_items_two">Humidity: {data.main.humidity}%</p>
+                <p className="weather_items_two">Wind Speed: {data.wind.speed} m/s</p>
+            </div>
+            <div className="flex">
+                <p className="weather_items_three">Sunrise: {formatTime(data.sys.sunrise)}</p>
+                <p className="weather_items_three">Sunset: {formatTime(data.sys.sunset)}</p>
+            </div>
         </div>
     );
 };
